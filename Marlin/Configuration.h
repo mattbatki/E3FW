edb952c1e371f3de2dd3b50665a0b2491a211343
mattbatki <matthewbatkiewicz@gmail.com>
2024-03-03 13:45:11 -0500

    date

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -128,7 +128,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 Pro 2/27/24"
+#define CUSTOM_MACHINE_NAME "Ender-3 Pro 3/3/24"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
